4dd94cab7d6472ac523b56472fe22a88ce1b4078
MarieQuesnel <marie.quesnel@outlook.com>
2020-05-11 10:52:37 +0200

    11/05/2020

--- Index.php
+++ Index.php
@@ -1,4 +1,7 @@
-
+<?php
+    $manager = new PersonnageManager($db = newPDO('127.0.0.1','root',''));
+    $manager = new PersonnageManager($db);
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
   <head>
